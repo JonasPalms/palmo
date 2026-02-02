afa8810e0d19db994a11645407de6d4bb82ff8f3
JonasPalms <jonas.p.sorensen@gmail.com>
2026-02-03 00:32:22 +0100

    added some more stuff lets gooo

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,17 @@ import { defineCollection, z } from "astro:content"
 
 const projects = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tech: z.array(z.string()),
-    github: z.string().url().optional(),
-    live: z.string().url().optional(),
-    date: z.coerce.date(),
-    featured: z.boolean().default(false),
-    cover: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tech: z.array(z.string()),
+      github: z.string().url().optional(),
+      live: z.string().url().optional(),
+      date: z.coerce.date(),
+      featured: z.boolean().default(false),
+      cover: image().optional(),
+    }),
 })
 
 export const collections = {
